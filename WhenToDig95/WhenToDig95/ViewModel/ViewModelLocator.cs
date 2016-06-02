c435cb1be40ebce4d49e9292af7f990fa58209a4
ivNetAdmin <bp@ivnet.co.uk>
2016-06-02 15:28:02 +0100

    Update ViewModelLocator.cs
    ViewModelLocator

--- WhenToDig95/WhenToDig95/ViewModel/ViewModelLocator.cs
+++ WhenToDig95/WhenToDig95/ViewModel/ViewModelLocator.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace WhenToDig95.ViewModel
 {
-    class ViewModelLocator
+    public class ViewModelLocator
     {
+        static ViewModelLocator()
+    {
+        ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        SimpleIoc.Default.Register<MainViewModel>();
+    }
+ 
+    /// <summary>
+    /// Gets the Main property.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+        "CA1822:MarkMembersAsStatic",
+        Justification = "This non-static member is needed for data binding purposes.")]
+    public MainViewModel Main
+    {
+        get
+        {
+            return ServiceLocator.Current.GetInstance<MainViewModel>();
+        }
+    }
     }
 }
