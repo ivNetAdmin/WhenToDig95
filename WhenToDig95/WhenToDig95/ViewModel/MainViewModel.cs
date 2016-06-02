3bddf3f2ebac064a6c96c5e09c2228f22b073fe1
ivNetAdmin <bp@ivnet.co.uk>
2016-06-02 15:28:50 +0100

    Update MainViewModel.cs
    MainViewModel

--- WhenToDig95/WhenToDig95/ViewModel/MainViewModel.cs
+++ WhenToDig95/WhenToDig95/ViewModel/MainViewModel.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace WhenToDig95.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ViewModelBase
+{
+    /// <summary>
+    /// The <see cref="ClickCount" /> property's name.
+    /// </summary>
+    public const string ClickCountPropertyName = "ClickCount";
+ 
+    private int _clickCount;
+ 
+    /// <summary>
+    /// Sets and gets the ClickCount property.
+    /// Changes to that property's value raise the PropertyChanged event. 
+    /// </summary>
+    public int ClickCount
     {
+        get
+        {
+            return _clickCount;
+        }
+        set
+        {
+            if (Set(() => ClickCount, ref _clickCount, value))
+            {
+                RaisePropertyChanged(() => ClickCountFormatted);
+            }
+        }
+    }
+ 
+    public string ClickCountFormatted
+    {
+        get
+        {
+            return string.Format("The button was clicked {0} time(s)", ClickCount);
+        }
+    }
+ 
+    private RelayCommand _incrementCommand;
+ 
+    /// <summary>
+    /// Gets the IncrementCommand.
+    /// </summary>
+    public RelayCommand IncrementCommand
+    {
+        get
+        {
+            return _incrementCommand
+                ?? (_incrementCommand = new RelayCommand(
+                () =>
+                {
+                    ClickCount++;
+                }));
+        }
     }
 }
