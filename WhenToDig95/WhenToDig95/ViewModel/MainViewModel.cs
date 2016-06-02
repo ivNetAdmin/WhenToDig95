7bbf4086abe8a4d08294bcaa2c2d7bd611167338
ivNetAdmin <bp@ivnet.co.uk>
2016-06-02 15:25:45 +0100

    mvvm light

---
+++ WhenToDig95/WhenToDig95/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenToDig95.ViewModel
+{
+    class MainViewModel
+    {
+    }
+}
